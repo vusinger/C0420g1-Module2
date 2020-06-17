f823bc2ddd64cac6db45f3089f75293164e35f6e
vusinger <vusinger@gmail.com>
2020-06-17 10:02:10 +0700

    p

---
+++ 11. DSAStackQueue/BaiTap/src/queue_danh_sach_lien_ket_vong/Solution.java
@@ -0,0 +1,26 @@
+package queue_danh_sach_lien_ket_vong;
+
+public class Solution {
+    public static void main(String[] args) {
+        // Create a queue and initialize front and rear
+        Queue  q = new Queue();
+        // Inserting elements in Circular Queue
+        q.enQueue(14);
+        q.enQueue(22);
+        q.enQueue(6);
+
+        // Display elements present in Circular Queue
+        q.displayQueue();
+
+        // Deleting elements from Circular Queue
+        System.out.println("Deleted value = " + q.deQueue());
+        System.out.println("Deleted value = " + q.deQueue());
+
+        // Remaining elements in Circular Queue
+        q.displayQueue();
+
+        q.enQueue(9);
+        q.enQueue(20);
+        q.displayQueue();
+    }
+}
